4c059883cb8afa650a015940b70d10f9b0332f67
De Light <ducnm.nguyen@gmail.com>
2024-09-28 16:39:56 +0700

    Update Structure
    Update Structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductListComponent } from "./components/product-list/product-list.component";
+import { CartComponent } from "./components/cart/cart.component";
+import { ProductItemComponent } from "./components/product-item/product-item.component";
+import { HeaderComponent } from "./layout/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ProductItemComponent,ProductListComponent,CartComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
