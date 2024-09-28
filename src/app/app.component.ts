37f1a1f81a536324c9bc21632f7f3d076ac24a83
De Light <ducnm.nguyen@gmail.com>
2024-09-28 09:58:45 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'nd0067-c3-angular-fundamentals-project-starter';
+}
